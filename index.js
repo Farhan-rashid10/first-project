bfbedaf426b69741c62c12c90bc6929763824d7b
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-25 03:15:40 +0300

    m

---
+++ index.js
@@ -0,0 +1,14 @@
+ let sub = document.getElementById("bg")
+//  let input = getElementById("input")
+document.getElementById("btn").addEventListener("click" , function(){
+    
+
+    // const nameInput = document.getElementById('input').value;
+    // if (nameInput.value === '') {
+    //     alert('Please fill in the name field.');
+    // } else {
+    //     alert('Form submitted with name: ' + nameInput);
+    // }
+} )  
+
+   
