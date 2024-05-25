a022c03ff894c61d877136531a4250a2cf72592a
Farhan-rashid10 <farkaf07@gmail.com>
2024-05-25 18:04:40 +0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-//  let sub = document.getElementById("bg")
-// //  let input = getElementById("input")
-// document.getElementById("btn").addEventListener("click" , function(){
-    
+ let bg = document.getElementById("bg")
+ let input1 = document.getElementById("input1")
+ let input2 = document.getElementById("input2")
+ let input3 = document.getElementById("input3")
+ let input4 = document.getElementById("input4")
+ let input5 = document.getElementById("input5")
 
-//     const nameInput = document.getElementById('input').value;
-//     if (nameInput.value === '') {
-//         alert('Please fill in the name field.');
-//     } else {
-//         alert('Form submitted with name: ' + nameInput);
-//     }
-// } )  
+
+ let button = document.getElementById("btn")
+button.addEventListener("click" , function(){
+
+   
+    if (input1.value === "" || input2.value === "" || input3.value === "" || input4.value === "" || input5.value === "") 
+        {
+        alert('Please fill other fields.');
+       } else {
+      input.textContent = input.value;
+      sub.appendChild(input);
+      alert("submited")
+
+    }
+} )  
 
    
